fa3207a36710c9778baf57ea050502ecb41ecb93
aggelos950 <102741602+aggelos950@users.noreply.github.com>
2024-02-20 16:35:29 +0200

    hope it works
    efewfe

---
+++ MediaLibrary!/web/sites/default/files/js/js_qm0LMrCz-GSwwpbtKSQBIoIeSLKaKKrjy7ob0O60CQo.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mytheme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
+;
